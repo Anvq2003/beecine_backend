a0ccbf8486e727ee4d6d920e126495b8e892ce88
an.voquoc <an.voquoc@alta.com.vn>
2023-11-22 09:42:43 +0700

    Fix import statements and add new methods for OTP and password management

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,11 +1,14 @@
-require("dotenv").config();
+require('dotenv').config();
 const { JWT_ACCESS_KEY, JWT_REFRESH_KEY, ACCESS_TOKEN_EXPIRATION, REFRESH_TOKEN_EXPIRATION } =
   process.env;
-const admin = require("firebase-admin");
-const jwt = require("jsonwebtoken");
-const UserModel = require("../models/user");
-const RefreshTokenModel = require("../models/refreshToken");
-const { getMillisecondsInDuration } = require("../utils/format");
+const admin = require('firebase-admin');
+const jwt = require('jsonwebtoken');
+const UserModel = require('../models/user');
+const OTPModel = require('../models/otp');
+const RefreshTokenModel = require('../models/refreshToken');
+const { getMillisecondsInDuration } = require('../utils/format');
+const generateOTP = require('../helpers/generateOTP');
+const { sendMail } = require('../helpers/sender');
 
 class AuthController {
   async signUp(req, res) {
@@ -13,12 +16,12 @@ class AuthController {
       const { token, name } = req.body;
       const decodedToken = await this.verifyTokenFirebase(token);
 
-      if (!decodedToken) return res.status(404).json({ message: "User not found" });
+      if (!decodedToken) return res.status(404).json({ message: 'User not found' });
 
       await admin.auth().updateUser(decodedToken.uid, { displayName: name });
       await this.createUser({ ...decodedToken, name });
 
-      return res.status(200).json("Sign up successfully");
+      return res.status(200).json('Sign up successfully');
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
@@ -28,10 +31,10 @@ class AuthController {
     try {
       const { token } = req.body;
       const decodedToken = await this.verifyTokenFirebase(token);
-      if (!decodedToken) return res.status(404).json({ message: "User not found" });
+      if (!decodedToken) return res.status(404).json({ message: 'User not found' });
 
       const user = await UserModel.findOne({ uid: decodedToken.uid });
-      if (!user) return res.status(404).json({ message: "User not found" });
+      if (!user) return res.status(404).json({ message: 'User not found' });
 
       const { accessToken, refreshToken } = this.createTokens(user);
 
@@ -51,12 +54,12 @@ class AuthController {
     try {
       const { token } = req.body;
       const decodedToken = await this.verifyTokenFirebase(token);
-      if (!decodedToken) return res.status(404).json({ message: "User not found" });
+      if (!decodedToken) return res.status(404).json({ message: 'User not found' });
 
       const user = await UserModel.findOne({ uid: decodedToken.uid });
-      if (!user) return res.status(404).json({ message: "User not found" });
+      if (!user) return res.status(404).json({ message: 'User not found' });
 
-      if (user.role !== "ADMIN") return res.status(404).json({ message: "You are not admin" });
+      if (user.role !== 'ADMIN') return res.status(404).json({ message: 'You are not admin' });
 
       const { accessToken, refreshToken } = this.createTokens(user);
 
@@ -72,11 +75,85 @@ class AuthController {
     }
   }
 
+  async sendOtpToEmail(req, res) {
+    try {
+      const { email } = req.body;
+      const user = await admin.auth().getUserByEmail(email);
+      if (!user) return res.status(404).json({ message: 'User not found' });
+      const otpCode = generateOTP();
+      const now = Date.now();
+      await OTPModel.findByIdAndDelete({ email });
+      const otp = await OTPModel.create({
+        email,
+        code: otpCode,
+        issuedAt: now,
+        expiresAt: now + 600000,
+      });
+
+      const info = {
+        from: {
+          name: 'Beecine',
+          address: process.env.EMAIL,
+        },
+        to: user.email,
+        subject: 'Mã OTP',
+        text: `Mã OTP của bạn là ${otpCode} và sẽ hết hạn trong 10 phút`,
+      };
+      await sendMail(info);
+      res.status(200).json({ message: 'Send OTP successfully' });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+
+  async verifyOtp(req, res) {
+    try {
+      const { email, otp } = req.body;
+      if(!email || !otp) return res.status(404).json({ message: 'Email and OTP are required' });
+      const now = Date.now();
+      const data = await OTPModel.findOne({ email });
+      if (!data) return res.status(404).json({ message: 'OTP not found' });
+      if (data.expiresAt < now) return res.status(404).json({ message: 'OTP has expired' });
+      if (data.code !== otp) return res.status(404).json({ message: 'OTP is incorrect' });
+      await OTPModel.findByIdAndDelete(data._id);
+      res.status(200).json({ message: 'Verify OTP successfully' });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+
+  async changePassword(req, res) {
+    try {
+      const { uid, oldPassword, newPassword } = req.body;
+      if (oldPassword === newPassword)
+        return res
+          .status(500)
+          .json({ message: 'New password must be different from old password' });
+
+      const user = await admin.auth().getUser(uid);
+      if (!user) return res.status(404).json({ message: 'User not found' });
+
+      const validPassword = await admin.auth().verifyPassword(oldPassword, user.passwordHash);
+
+      if (!validPassword) {
+        return res.status(500).json({ message: 'Old password is incorrect' });
+      }
+
+      await admin.auth().updateUser(uid, { password: newPassword });
+
+      res.status(200).json('Change password successfully');
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  }
+
+  async forgotPassword(req, res) {}
+
   async signInWithGoogle(req, res) {
     try {
       const { token } = req.body;
       const decodedToken = await this.verifyTokenFirebase(token);
-      if (!decodedToken) return res.status(404).json({ message: "User not found" });
+      if (!decodedToken) return res.status(404).json({ message: 'User not found' });
 
       let user = await UserModel.findOne({ email: decodedToken.email });
       if (!user) {
@@ -103,20 +180,20 @@ class AuthController {
   async refreshToken(req, res) {
     try {
       const { refreshToken } = req.body;
-      if (!refreshToken) return res.status(404).json({ message: "Refresh token is required" });
+      if (!refreshToken) return res.status(404).json({ message: 'Refresh token is required' });
 
       const storedRefreshToken = await RefreshTokenModel.findOne({ token: refreshToken });
       if (!storedRefreshToken)
-        return res.status(404).json({ message: "Refresh  1223 token not found" });
+        return res.status(404).json({ message: 'Refresh  1223 token not found' });
 
       const currentTimestamp = Date.now();
 
       if (storedRefreshToken.expiresAt < currentTimestamp) {
-        return res.status(404).json({ message: "Refresh token has expired" });
+        return res.status(404).json({ message: 'Refresh token has expired' });
       }
 
       const decodedToken = jwt.verify(refreshToken, JWT_REFRESH_KEY);
-      if (!decodedToken) return res.status(404).json({ message: "Refresh token is invalid" });
+      if (!decodedToken) return res.status(404).json({ message: 'Refresh token is invalid' });
 
       const newTokens = this.createTokens(decodedToken);
       this.createNewRefreshToken(newTokens.refreshToken, decodedToken._id);
@@ -134,8 +211,8 @@ class AuthController {
   async getProfile(req, res) {
     try {
       const { _id } = req.user;
-      const user = await UserModel.findById(_id).populate("subscription");
-      if (!user) return res.status(404).json({ message: "User not found" });
+      const user = await UserModel.findById(_id).populate('subscription');
+      if (!user) return res.status(404).json({ message: 'User not found' });
       res.status(200).json(user);
     } catch (error) {
       res.status(500).json({ message: error.message });
@@ -149,9 +226,9 @@ class AuthController {
   async createUser(payload) {
     const data = Object.assign(
       {
-        uid: "",
-        email: "",
-        name: "",
+        uid: '',
+        email: '',
+        name: '',
         imageUrl: null,
       },
       payload,
@@ -178,6 +255,7 @@ class AuthController {
     const payload = {
       _id: user._id,
       role: user.role,
+      uid: user.uid,
     };
 
     const accessToken = jwt.sign(payload, JWT_ACCESS_KEY, { expiresIn: ACCESS_TOKEN_EXPIRATION });
---
+++ models/otp.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+const otpSchema = new mongoose.Schema({
+  code: { type: String, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  issuedAt: { type: Number, required: true },
+  expiresAt: { type: Number, required: true },
+});
+
+const OTP = mongoose.model('Otp', otpSchema);
+module.exports = OTP;
